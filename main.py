7077633ad81749ed426195406cd716aebff97acf
Carlos Daniel <danieldpaula07@gmail.com>
2026-06-09 20:28:10 -0300

    feat: cria estrutura inicial do sistema de campeonato

---
+++ main.py
@@ -0,0 +1,22 @@
+times = []
+
+while True:
+    print("\n=== CAMPEONATO BRASILEIRO ===")
+    print("1 - Cadastrar time")
+    print("2 - Listar times")
+    print("3 - Sair")
+
+    op = input("Escolha uma opção: ")
+
+    if op == "1":
+        print("Funcionalidade em desenvolvimento")
+
+    elif op == "2":
+        print("Funcionalidade em desenvolvimento")
+
+    elif op == "3":
+        print("Sistema encerrado.")
+        break
+
+    else:
+        print("Opção inválida.")
